4facf3d8ed8b50a8f8053d409bc0a0919772a383
Marek Maskarinec <fazolovy.kotlik@gmail.com>
2021-04-28 11:34:00 +0200

    add hentai

--- content/etc.md
+++ content/etc.md
@@ -2,3 +2,4 @@
 
 #### [hammer editor on linux](hammer.html)
 #### [rick roll](https://www.youtube.com/watch?v=dQw4w9WgXcQ)
+#### [hentai.sh](https://marekmaskarinec.github.io/hentai.sh.html)
---
+++ files/hentai.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+JSON=~/.cache/hentai.json
+if [ -f $JSON ]; then if test `find $JSON -mmin +60`; then curl -s -A "hentai.sh v0.1 - linux" "https://www.reddit.com/r/hentai.json?limit=100" | sed 's/\\n/ /g' > $JSON; fi	
+else curl -s -A "hentai.sh v0.1 - linux" "https://www.reddit.com/r/hentai.json?limit=100" | sed 's/\\n/ /g' > $JSON; fi
+R="$(($RANDOM % $(cat $JSON | jq '.data.children | length')+1))"
+URL=$(cat $JSON | jq ".data.children[$R].data.url" | sed 's/\"//g')
+echo $URL
+cat $JSON | jq ".data.children[$R].data.title" --raw-output
+if [[ "$URL" == *".gif"* ]]; then mpv --loop $URL; exit 0; fi 
+curl -sA "hentai.sh v0.1" $URL | feh -F -
